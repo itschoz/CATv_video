d98ff9b78a21b527466d442b805ab04edd0035c3
itschoz <yongbum9344@gmail.com>
2017-09-30 21:56:03 +0900

    First Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cho/catvehicle_ws/src
+# Build directory: /home/cho/catvehicle_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(obstaclestopper)
+SUBDIRS(catvehicle)
---
+++ build/catvehicle/CMakeFiles/distanceEstimator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/distanceEstimator.dir/src/distanceEstimator.cpp.o"
+  "/home/cho/catvehicle_ws/devel/lib/catvehicle/distanceEstimator.pdb"
+  "/home/cho/catvehicle_ws/devel/lib/catvehicle/distanceEstimator"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/distanceEstimator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
